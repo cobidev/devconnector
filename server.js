c3223b4e185887d107948f92939e9dc0b6015609
Jacobo Martinez <jacobojavier98@gmail.com>
2019-06-05 20:10:38 -0400

    Create Routes files with Express Router

--- server.js
+++ server.js
@@ -8,5 +8,11 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API RUNNING'));
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 const PORT = process.env.port || 5000;
 app.listen(PORT, err => console.log(`Server started on port: ${PORT}`));
