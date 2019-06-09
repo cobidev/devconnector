2d4ea0b3831fd5d166107447a5f5625b81d896be
Jacobo Martinez <jacobojavier98@gmail.com>
2019-06-08 22:36:59 -0400

    Create GET current profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const { verifyAuth } = require('../../middleware/auth');
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => {
-  res.send('Profile route');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route   GET api/profile/me
+// @desc    Get current user profile
+// @access  Private
+router.get('/me', verifyAuth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.userID }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: 'Server Error.' });
+  }
 });
 
 module.exports = router;
