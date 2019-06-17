e5c11c189afe1cedc2bfae4c9ae738b14d9b82c6
Jacobo Martinez <jacobojavier98@gmail.com>
2019-06-17 19:29:15 -0400

    add Initial Components (Navbar, Landing)

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 import './App.css';
 
 const App = () => (
   <div>
-    <h1>App</h1>
+    <Navbar />
+    <Landing />
   </div>
 );
 
