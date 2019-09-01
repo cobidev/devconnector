33abaad325aa8d00a4d9937fc6e0078b4a19b4d4
Jacobo Martinez <jacobojavier98@gmail.com>
2019-08-31 23:15:46 -0400

    Setup Create Profile Action (Redux)

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { setAlert } from './alert';
 
 // Get current user profile
 export const getCurrentProfile = () => async dispatch => {
@@ -10,6 +11,49 @@ export const getCurrentProfile = () => async dispatch => {
       payload: res.data
     });
   } catch (err) {
+    dispatch({
+      type: 'PROFILE_ERROR',
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Create or Update profile
+export const createProfile = (
+  formData,
+  history,
+  edit = false
+) => async dispatch => {
+  try {
+    // prepare headers for post request
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+    // POST profile data
+    const res = await axios.post('/api/profile', formData, config);
+
+    // Dispatch profile into redux state
+    dispatch({
+      type: 'GET_PROFILE',
+      payload: res.data
+    });
+
+    // if edit param is false, dispatch diferent message
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+
+    // redirect if edit param is false
+    if (!edit) {
+      history.push('/dashboard');
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
     dispatch({
       type: 'PROFILE_ERROR',
       payload: { msg: err.response.statusText, status: err.response.status }
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -10,14 +10,14 @@ const Dashboard = ({ auth, currentProfile, getCurrentProfile }) => {
 
   // Print spinner if current profile is loading
   return currentProfile.loading && currentProfile.profile === null ? (
-    <i class="fa fa-spinner fa-spin fa-4x" />
+    <i className='fa fa-spinner fa-spin fa-4x' />
   ) : (
     // Print Dashboard page
     <section>
-      <h1 className="large text-primary">Dashboard</h1>
+      <h1 className='large text-primary'>Dashboard</h1>
 
-      <p className="lead">
-        <i className="fas fa-user" /> Welcome {auth.user && auth.user.name}
+      <p className='lead'>
+        <i className='fas fa-user' /> Welcome {auth.user && auth.user.name}
       </p>
 
       {currentProfile.profile !== null ? (
@@ -25,7 +25,7 @@ const Dashboard = ({ auth, currentProfile, getCurrentProfile }) => {
       ) : (
         <>
           <p>You have not yet setup a profile, please add some info</p>
-          <Link to="/create/profile" className="btn btn-primary my-1">
+          <Link to='/create/profile' className='btn btn-primary my-1'>
             Create profile
           </Link>
         </>
